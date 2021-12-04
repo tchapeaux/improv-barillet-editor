26aa04db66a2710f31a6a05330a5a869e5f8b4fb
Thomas Chapeaux <tom@wooclap.com>
2021-12-04 16:45:43 +0100

    linter

--- vite.config.js
+++ vite.config.js
@@ -5,12 +5,12 @@ import reactRefresh from "@vitejs/plugin-react-refresh";
 export default defineConfig({
   plugins: [reactRefresh()],
   build: {
-    outDir: "build"
+    outDir: "build",
   },
   server: {
     strictPort: true,
     hmr: {
-      port: 443 // Run the websocket server on the SSL port
-    }
-  }
+      port: 443, // Run the websocket server on the SSL port
+    },
+  },
 });
